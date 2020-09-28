85f7659d0bd37fe4d908e7a9276e9906cf64a9a6
Jon Hill <jondhill1984@gmail.com>
2020-09-28 16:30:12 +0300

    Add initial nav styles and placeholder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,42 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
+const NavStyles = styled.div`
+    display: grid;
+    grid-template-columns: 60% 40%;
+    font-size: 1.2rem;
+    font-weight: 100;
+    border: solid 1px red;
+  div {
+
+  }
+  ul {
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    text-align: center;
+  }
+  @media(max-width: 1100px) {
+    grid-template-columns: 40% 60%;
+    ul {
+      margin-right: 20px;
+    }
+    li {
+      font-size: 0.8rem;
+    }
+  }
+`;
+
+
 
 export default function Nav() {
     return (
-    <div>
+    <NavStyles>
+      <div>Phone number placeholder</div>
       <ul>
         <li>
           <Link to="/importantInfo">Important Info</Link>
@@ -20,6 +52,6 @@ export default function Nav() {
           <Link to="/instagram">Instagram</Link>
         </li>
       </ul>
-    </div>
+    </NavStyles>
     )
 }
